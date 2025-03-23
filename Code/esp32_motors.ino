8454ade38c463967cb64e4585a645a54071d29d0
cruzryan <rncb0963@gmail.com>
2025-03-23 13:50:17 -0600

    HW+SW control

--- Code/esp32_motors.ino
+++ Code/esp32_motors.ino
@@ -2,11 +2,15 @@
 #include <WiFi.h>
 #include <esp_http_server.h>
 #include <ArduinoJson.h>
+#include <soc/gpio_reg.h>  // For direct GPIO register access
 
 // WiFi credentials
 #define SSID "TT"
 #define PASSWORD "12345678"
 #define PORT 82
+
+// Number of motors (define early for scope)
+const int NUM_MOTORS = 8;
 
 // Control parameters (shared across all motors)
 float kp = 0.9;
@@ -22,6 +26,13 @@ const int COUNTS_PER_REV = 1975;
 // Timing
 unsigned long lastTime = 0;
 const unsigned long dt = 2;
+
+// Software PWM variables for motors 4-7
+volatile int dutyCycles[NUM_MOTORS][2] = {0};  // Duty cycles for IN1, IN2 per motor
+hw_timer_t *timer = NULL;
+volatile int pwmCounter = 0;
+const int PWM_RESOLUTION = 255;  // Full 8-bit resolution for motor control
+const int PWM_FREQ = 200;        // Reduced to 200 Hz to lower interrupt rate (200 * 255 = 51,000 Hz)
 
 // HTTP server handle
 httpd_handle_t server = NULL;
@@ -39,11 +50,9 @@ struct Motor {
     bool controlEnabled;
 };
 
-// Number of motors (adjust as needed)
-const int NUM_MOTORS = 8;
 Motor motors[NUM_MOTORS];
 
-// Interrupt Handlers
+// Interrupt Handlers for Encoders
 void IRAM_ATTR handleEncoderA(void* arg) {
     Motor* motor = (Motor*)arg;
     bool encAVal = digitalRead(motor->ENCODER_A);
@@ -66,19 +75,58 @@ void IRAM_ATTR handleEncoderB(void* arg) {
     }
 }
 
+// Software PWM Interrupt Handler for Motors 4-7 (optimized with direct register access)
+void IRAM_ATTR onTimer() {
+    pwmCounter = (pwmCounter + 1) % PWM_RESOLUTION;
+    for (int i = 4; i < NUM_MOTORS; i++) {
+        Motor &motor = motors[i];
+        if (motor.IN1 != -1) {
+            if (pwmCounter < dutyCycles[i][0]) {
+                REG_WRITE(GPIO_OUT_W1TS_REG, 1 << motor.IN1);  // Set HIGH
+            } else {
+                REG_WRITE(GPIO_OUT_W1TC_REG, 1 << motor.IN1);  // Set LOW
+            }
+        }
+        if (motor.IN2 != -1) {
+            if (pwmCounter < dutyCycles[i][1]) {
+                REG_WRITE(GPIO_OUT_W1TS_REG, 1 << motor.IN2);  // Set HIGH
+            } else {
+                REG_WRITE(GPIO_OUT_W1TC_REG, 1 << motor.IN2);  // Set LOW
+            }
+        }
+    }
+}
+
 // Motor Control Functions
 void setMotor(int motorIndex, int power) {
     Motor &motor = motors[motorIndex];
-    //Serial.printf("Motor %d: Power = %d, IN1 = %d, IN2 = %d\n", motorIndex, power, motor.IN1, motor.IN2);
-    if (power == 0) {
-        ledcWrite(motor.IN1, 255);  // Brake
-        ledcWrite(motor.IN2, 255);
-    } else if (power > 0) {
-        ledcWrite(motor.IN2, 0);
-        ledcWrite(motor.IN1, power);
+    if (motorIndex < 4) {
+        // Hardware PWM for motors 0-3 using LEDC
+        int channel1 = motorIndex * 2;      // Channels 0, 2, 4, 6
+        int channel2 = motorIndex * 2 + 1;  // Channels 1, 3, 5, 7
+
+        if (power == 0) {
+            ledcWrite(motor.IN1, 255);  // Brake
+            ledcWrite(motor.IN2, 255);
+        } else if (power > 0) {
+            ledcWrite(motor.IN2, 0);
+            ledcWrite(motor.IN1, power);
+        } else {
+            ledcWrite(motor.IN1, 0);
+            ledcWrite(motor.IN2, -power);
+        }
     } else {
-        ledcWrite(motor.IN1, 0);
-        ledcWrite(motor.IN2, -power);
+        // Software PWM for motors 4-7
+        if (power == 0) {
+            dutyCycles[motorIndex][0] = PWM_RESOLUTION;  // Brake
+            dutyCycles[motorIndex][1] = PWM_RESOLUTION;
+        } else if (power > 0) {
+            dutyCycles[motorIndex][0] = power;  // Already 0-255
+            dutyCycles[motorIndex][1] = 0;
+        } else {
+            dutyCycles[motorIndex][0] = 0;
+            dutyCycles[motorIndex][1] = -power;  // Already 0-255
+        }
     }
 }
 
@@ -114,8 +162,6 @@ void controlMotor(int motorIndex) {
     }
 
     int power = computePower(error, errorDelta) + ki * motor.integralError;
-    //Serial.printf("Motor %d: targetPos = %ld, encoderPos = %ld, error = %ld, power = %d\n", 
-       //           motorIndex, motor.targetPos, motor.encoderPos, error, power);
     setMotor(motorIndex, power);
 }
 
@@ -150,7 +196,85 @@ static esp_err_t set_angle_handler(httpd_req_t *req) {
         if (httpd_query_key_value(query, "a", buf, sizeof(buf)) == ESP_OK) {
             int angle = atoi(buf);
             motors[motorIndex].targetPos = angle * COUNTS_PER_REV / 360;
-            //Serial.printf("Motor %d: Set angle = %d, targetPos = %ld\n", motorIndex, angle, motors[motorIndex].targetPos);
+        }
+    }
+    return httpd_resp_send(req, "K", 1);
+}
+
+static esp_err_t set_angles_handler(httpd_req_t *req) {
+    char query[256];
+    if (httpd_req_get_url_query_str(req, query, sizeof(query)) == ESP_OK) {
+        char buf[20];
+        for (int i = 0; i < NUM_MOTORS; i++) {
+            char param[3];
+            snprintf(param, sizeof(param), "a%d", i);
+            if (httpd_query_key_value(query, param, buf, sizeof(buf)) == ESP_OK) {
+                int angle = atoi(buf);
+                motors[i].targetPos = angle * COUNTS_PER_REV / 360;
+            }
+        }
+    }
+    return httpd_resp_send(req, "K", 1);
+}
+
+static esp_err_t set_all_pins_handler(httpd_req_t *req) {
+    char query[1024];
+    if (httpd_req_get_url_query_str(req, query, sizeof(query)) == ESP_OK) {
+        char buf[20];
+        for (int i = 0; i < NUM_MOTORS; i++) {
+            Motor &motor = motors[i];
+            int new_ENCODER_A = motor.ENCODER_A;
+            int new_ENCODER_B = motor.ENCODER_B;
+            int new_IN1 = motor.IN1;
+            int new_IN2 = motor.IN2;
+
+            char param[12];
+            snprintf(param, sizeof(param), "ENCODER_A%d", i);
+            if (httpd_query_key_value(query, param, buf, sizeof(buf)) == ESP_OK) new_ENCODER_A = atoi(buf);
+            snprintf(param, sizeof(param), "ENCODER_B%d", i);
+            if (httpd_query_key_value(query, param, buf, sizeof(buf)) == ESP_OK) new_ENCODER_B = atoi(buf);
+            snprintf(param, sizeof(param), "IN1_%d", i);
+            if (httpd_query_key_value(query, param, buf, sizeof(buf)) == ESP_OK) new_IN1 = atoi(buf);
+            snprintf(param, sizeof(param), "IN2_%d", i);
+            if (httpd_query_key_value(query, param, buf, sizeof(buf)) == ESP_OK) new_IN2 = atoi(buf);
+
+            if (motor.ENCODER_A != -1) detachInterrupt(digitalPinToInterrupt(motor.ENCODER_A));
+            if (motor.ENCODER_B != -1) detachInterrupt(digitalPinToInterrupt(motor.ENCODER_B));
+
+            motor.ENCODER_A = new_ENCODER_A;
+            motor.ENCODER_B = new_ENCODER_B;
+            motor.IN1 = new_IN1;
+            motor.IN2 = new_IN2;
+
+            if (motor.ENCODER_A != -1) {
+                pinMode(motor.ENCODER_A, INPUT_PULLUP);
+                if (digitalPinToInterrupt(motor.ENCODER_A) != NOT_AN_INTERRUPT) {
+                    attachInterruptArg(digitalPinToInterrupt(motor.ENCODER_A), handleEncoderA, &motor, CHANGE);
+                }
+            }
+            if (motor.ENCODER_B != -1) {
+                pinMode(motor.ENCODER_B, INPUT_PULLUP);
+                if (digitalPinToInterrupt(motor.ENCODER_B) != NOT_AN_INTERRUPT) {
+                    attachInterruptArg(digitalPinToInterrupt(motor.ENCODER_B), handleEncoderB, &motor, CHANGE);
+                }
+            }
+
+            if (motor.IN1 != -1) {
+                pinMode(motor.IN1, OUTPUT);
+                digitalWrite(motor.IN1, LOW);
+                if (i < 4) {  // Hardware PWM for motors 0-3
+                    ledcAttachChannel(motor.IN1, 1000, 8, i * 2);  // Channels 0, 2, 4, 6
+                    ledcWrite(motor.IN1, 255);
+                }
+            }
+            if (motor.IN2 != -1) {
+                pinMode(motor.IN2, OUTPUT);
+                digitalWrite(motor.IN2, LOW);
+                if (i < 4) {  // Hardware PWM for motors 0-3
+                    ledcAttachChannel(motor.IN2, 1000, 8, i * 2 + 1);  // Channels 1, 3, 5, 7
+                    ledcWrite(motor.IN2, 255);
+                }
+            }
         }
     }
     return httpd_resp_send(req, "K", 1);
@@ -171,7 +295,6 @@ static esp_err_t set_pins_handler(httpd_req_t *req) {
         }
 
         Motor &motor = motors[motorIndex];
-
         int new_ENCODER_A = motor.ENCODER_A;
         int new_ENCODER_B = motor.ENCODER_B;
         int new_IN1 = motor.IN1;
@@ -182,57 +305,48 @@ static esp_err_t set_pins_handler(httpd_req_t *req) {
         if (httpd_query_key_value(query, "IN1", buf, sizeof(buf)) == ESP_OK) new_IN1 = atoi(buf);
         if (httpd_query_key_value(query, "IN2", buf, sizeof(buf)) == ESP_OK) new_IN2 = atoi(buf);
 
-        // Detach interrupts from old pins if they were previously set
-        if (motor.ENCODER_A != -1) {
-            detachInterrupt(digitalPinToInterrupt(motor.ENCODER_A));
-        }
-        if (motor.ENCODER_B != -1) {
-            detachInterrupt(digitalPinToInterrupt(motor.ENCODER_B));
-        }
+        if (motor.ENCODER_A != -1) detachInterrupt(digitalPinToInterrupt(motor.ENCODER_A));
+        if (motor.ENCODER_B != -1) detachInterrupt(digitalPinToInterrupt(motor.ENCODER_B));
 
-        // Update pins
         motor.ENCODER_A = new_ENCODER_A;
         motor.ENCODER_B = new_ENCODER_B;
         motor.IN1 = new_IN1;
         motor.IN2 = new_IN2;
 
-        // Configure encoder pins
         if (motor.ENCODER_A != -1) {
             pinMode(motor.ENCODER_A, INPUT_PULLUP);
             if (digitalPinToInterrupt(motor.ENCODER_A) != NOT_AN_INTERRUPT) {
                 attachInterruptArg(digitalPinToInterrupt(motor.ENCODER_A), handleEncoderA, &motor, CHANGE);
-            } else {
-                //Serial.printf("ERROR: GPIO%d cannot be used for interrupts\n", motor.ENCODER_A);
             }
         }
         if (motor.ENCODER_B != -1) {
             pinMode(motor.ENCODER_B, INPUT_PULLUP);
             if (digitalPinToInterrupt(motor.ENCODER_B) != NOT_AN_INTERRUPT) {
                 attachInterruptArg(digitalPinToInterrupt(motor.ENCODER_B), handleEncoderB, &motor, CHANGE);
-            } else {
-                //Serial.printf("ERROR: GPIO%d cannot be used for interrupts\n", motor.ENCODER_B);
             }
         }
 
-        // Configure motor pins
         if (motor.IN1 != -1) {
             pinMode(motor.IN1, OUTPUT);
-            digitalWrite(motor.IN1, LOW);  // Set to LOW before PWM
-            ledcAttach(motor.IN1, 1000, 8);
-            if (ledcRead(motor.IN1) == 0) {
-                //Serial.printf("ERROR: Failed to attach PWM to GPIO%d (IN1)\n", motor.IN1);
+            digitalWrite(motor.IN1, LOW);
+            if (motorIndex < 4) {  // Hardware PWM for motors 0-3
+                ledcAttachChannel(motor.IN1, 1000, 8, motorIndex * 2);  // Channels 0, 2, 4, 6
+                ledcWrite(motor.IN1, 255);
             }
-            ledcWrite(motor.IN1, 255); // Brake
         }
         if (motor.IN2 != -1) {
             pinMode(motor.IN2, OUTPUT);
-            digitalWrite(motor.IN2, LOW);  // Set to LOW before PWM
-            ledcAttach(motor.IN2, 1000, 8);
-            if (ledcRead(motor.IN2) == 0) {
-                //Serial.printf("ERROR: Failed to attach PWM to GPIO%d (IN2)\n", motor.IN2);
+            digitalWrite(motor.IN2, LOW);
+            if (motorIndex < 4) {  // Hardware PWM for motors 0-3
+                ledcAttachChannel(motor.IN2, 1000, 8, motorIndex * 2 + 1);  // Channels 1, 3, 5, 7
+                ledcWrite(motor.IN2, 255);
             }
-            ledcWrite(motor.IN2, 255); // Brake
         }
+
+        char response[64];
+        snprintf(response, sizeof(response), "M%d: EA=%d, EB=%d, IN1=%d, IN2=%d", 
+                 motorIndex, motor.ENCODER_A, motor.ENCODER_B, motor.IN1, motor.IN2);
+        return httpd_resp_send(req, response, strlen(response));
     }
     return httpd_resp_send(req, "K", 1);
 }
@@ -297,7 +411,21 @@ static esp_err_t set_control_status_handler(httpd_req_t *req) {
         if (httpd_query_key_value(query, "status", buf, sizeof(buf)) == ESP_OK) {
             int status = atoi(buf);
             motors[motorIndex].controlEnabled = (status != 0);
+            if (motors[motorIndex].controlEnabled) {
+                // When enabling control, set targetPos to current encoderPos
+                // to prevent the motor from moving to a previous position
+                motors[motorIndex].targetPos = motors[motorIndex].encoderPos;
+                motors[motorIndex].lastError = 0;  // Reset PID terms
+                motors[motorIndex].integralError = 0;
+            } else {
+                // When disabling control, stop the motor immediately
+                setMotor(motorIndex, 0);
+            }
         }
+
+        char response[32];
+        snprintf(response, sizeof(response), "M%d: Control=%d", motorIndex, motors[motorIndex].controlEnabled);
+        return httpd_resp_send(req, response, strlen(response));
     }
     return httpd_resp_send(req, "K", 1);
 }
@@ -313,6 +441,14 @@ static esp_err_t reset_handler(httpd_req_t *req) {
                 return httpd_resp_send(req, "Invalid motor index", 18);
             }
             motors[motorIndex].encoderPos = 0;
+            motors[motorIndex].targetPos = 0;
+            motors[motorIndex].lastError = 0;  // Also reset lastError to avoid PID integral windup
+            motors[motorIndex].integralError = 0;  // Reset integral error for PID
+            // Debug output to confirm reset
+            char response[64];
+            snprintf(response, sizeof(response), "M%d: Pos=%ld, Target=%ld", 
+                     motorIndex, motors[motorIndex].encoderPos, motors[motorIndex].targetPos);
+            return httpd_resp_send(req, response, strlen(response));
         } else {
             return httpd_resp_send(req, "Missing motor index", 19);
         }
@@ -347,41 +483,45 @@ static esp_err_t events_handler(httpd_req_t *req) {
 }
 
 void setup() {
-    Serial.begin(115200);
+    pinMode(40, OUTPUT);
+    digitalWrite(40, LOW);
+
+    pinMode(39, OUTPUT);
+    digitalWrite(39, LOW);
+
+    for (int pin = 35; pin <= 42; pin++) {
+        pinMode(pin, OUTPUT);
+        digitalWrite(pin, LOW);
+    }
 
     // Initialize GPIO pins as OUTPUT and set to LOW
     for (int pin = 1; pin <= 2; pin++) {
         pinMode(pin, OUTPUT);
         digitalWrite(pin, LOW);
-        //Serial.printf("Set GPIO%d to LOW\n", pin);
     }
     for (int pin = 4; pin <= 18; pin++) {
         pinMode(pin, OUTPUT);
         digitalWrite(pin, LOW);
-        //Serial.printf("Set GPIO%d to LOW\n", pin);
     }
     pinMode(21, OUTPUT);
     digitalWrite(21, LOW);
-    //Serial.println("Set GPIO21 to LOW");
-    for (int pin = 35; pin <= 42; pin++) {
-        pinMode(pin, OUTPUT);
-        digitalWrite(pin, LOW);
-        //Serial.printf("Set GPIO%d to LOW\n", pin);
-    }
+    
     pinMode(47, OUTPUT);
     digitalWrite(47, LOW);
-    //Serial.println("Set GPIO47 to LOW");
     pinMode(48, OUTPUT);
     digitalWrite(48, LOW);
-    //Serial.println("Set GPIO48 to LOW");
 
     // Explicitly initialize motor control pins for FRONT RIGHT TURNING MOTOR
     pinMode(6, OUTPUT);
     digitalWrite(6, LOW);
-    //Serial.println("Set GPIO6 to LOW explicitly");
     pinMode(7, OUTPUT);
     digitalWrite(7, LOW);
-    //Serial.println("Set GPIO7 to LOW explicitly");
+
+    // Initialize timer for software PWM (motors 4-7)
+    timer = timerBegin(PWM_FREQ * PWM_RESOLUTION);  // 200 Hz * 255 steps = 51,000 Hz
+    timerAttachInterrupt(timer, &onTimer);          // Attach interrupt
+    timerAlarm(timer, 1, true, 0);                  // Trigger every tick, autoreload
+    timerStart(timer);  
 
     // Initialize motor structure
     for (int i = 0; i < NUM_MOTORS; i++) {
@@ -393,31 +533,32 @@ void setup() {
         motors[i].targetPos = 0;
         motors[i].lastError = 0;
         motors[i].integralError = 0;
-        motors[i].controlEnabled = false; // Control OFF for all motors at start
+        motors[i].controlEnabled = false;
     }
 
     // Connect to WiFi
     WiFi.begin(SSID, PASSWORD);
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
-        Serial.print(".");
     }
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());
 
     // Start HTTP server
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
     config.server_port = PORT;
-    config.core_id = 0;
+    config.core_id = 0;  // Run HTTP server on Core 0 to avoid interference with Core 1
     config.keep_alive_enable = true;
     if (httpd_start(&server, &config) == ESP_OK) {
+        httpd_uri_t set_all_pins_uri = { .uri = "/set_all_pins", .method = HTTP_GET, .handler = set_all_pins_handler, .user_ctx = NULL };
+        httpd_register_uri_handler(server, &set_all_pins_uri);
+
         httpd_uri_t set_control_params_uri = { .uri = "/set_control_params", .method = HTTP_GET, .handler = set_control_params_handler, .user_ctx = NULL };
         httpd_register_uri_handler(server, &set_control_params_uri);
 
         httpd_uri_t set_angle_uri = { .uri = "/set_angle", .method = HTTP_GET, .handler = set_angle_handler, .user_ctx = NULL };
         httpd_register_uri_handler(server, &set_angle_uri);
+
+        httpd_uri_t set_angles_uri = { .uri = "/set_angles", .method = HTTP_GET, .handler = set_angles_handler, .user_ctx = NULL };
+        httpd_register_uri_handler(server, &set_angles_uri);
 
         httpd_uri_t set_pins_uri = { .uri = "/set_pins", .method = HTTP_GET, .handler = set_pins_handler, .user_ctx = NULL };
         httpd_register_uri_handler(server, &set_pins_uri);
--- Code/quadpilot/body.py
+++ Code/quadpilot/body.py
@@ -11,14 +11,35 @@ class QuadPilotBody:
         url = f"http://{self.ip}:82/set_control_params?P={P}&I={I}&D={D}&dead_zone={dead_zone}&pos_thresh={pos_thresh}"
         self.session.get(url)
 
+    def set_all_control_params(self, P: float, I: float, D: float, dead_zone: int, pos_thresh: int):
+        """Set PID parameters, dead zone, and position threshold for all motors at once."""
+        url = f"http://{self.ip}:82/set_all_control_params?P={P}&I={I}&D={D}&dead_zone={dead_zone}&pos_thresh={pos_thresh}"
+        self.session.get(url)
+
     def set_angle(self, motor: int, a: int):
         """Set target angle in degrees for a specific motor."""
         url = f"http://{self.ip}:82/set_angle?motor={motor}&a={a}"
         self.session.get(url)
 
+    def set_angles(self, angles: list[int]):
+        """Set target angles in degrees for all 8 motors at once."""
+        if len(angles) != 8:
+            raise ValueError("Exactly 8 angles must be provided")
+        params = "&".join(f"a{i}={angle}" for i, angle in enumerate(angles))
+        url = f"http://{self.ip}:82/set_angles?{params}"
+        self.session.get(url)
+
     def set_pins(self, motor: int, ENCODER_A: int, ENCODER_B: int, IN1: int, IN2: int):
         """Set encoder and motor pins for a specific motor on the ESP32."""
         url = f"http://{self.ip}:82/set_pins?motor={motor}&ENCODER_A={ENCODER_A}&ENCODER_B={ENCODER_B}&IN1={IN1}&IN2={IN2}"
+        self.session.get(url)
+
+    def set_all_pins(self, pins: list[tuple[int, int, int, int]]):
+        """Set encoder and motor pins for all 8 motors at once."""
+        if len(pins) != 8:
+            raise ValueError("Exactly 8 motor pin configurations must be provided")
+        params = "&".join(f"ENCODER_A{i}={p[0]}&ENCODER_B{i}={p[1]}&IN1_{i}={p[2]}&IN2_{i}={p[3]}" for i, p in enumerate(pins))
+        url = f"http://{self.ip}:82/set_all_pins?{params}"
         self.session.get(url)
 
     def get_angle(self, motor: int) -> float:
@@ -36,7 +57,17 @@ class QuadPilotBody:
         url = f"http://{self.ip}:82/set_control_status?motor={motor}&status={1 if status else 0}"
         self.session.get(url)
 
+    def set_all_control_status(self, status: bool):
+        """Enable or disable control for all motors at once (True = on, False = off)."""
+        url = f"http://{self.ip}:82/set_all_control_status?status={1 if status else 0}"
+        self.session.get(url)
+
     def reset(self, motor: int):
         """Reset the encoder position to 0 for a specific motor."""
         url = f"http://{self.ip}:82/reset?motor={motor}"
+        self.session.get(url)
+
+    def reset_all(self):
+        """Reset the encoder positions to 0 for all motors at once."""
+        url = f"http://{self.ip}:82/reset_all"
         self.session.get(url)
